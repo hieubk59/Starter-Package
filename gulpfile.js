8888858687a56370fbcac3cf245e76c7bed10bb4
Hugh Hieu Ngoc Nguyen <Hugh.Nguyen@avtech.com.au>
2019-01-29 15:46:31 +0700

    update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var postcss = require('gulp-postcss');
 var cssnext = require('postcss-cssnext');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var jslint = require('gulp-jslint');
+var copy = require('gulp-copy');
+var csscompressor = require('gulp-copy');
+
 
 var config = {};
 config.src = './src';
@@ -15,24 +19,24 @@ config.buildTasks = ['sass', 'js'];
 
 // Compile SASS into CSS
 task('sass', function() {
-	var plugins = [ cssnext ];
-	return src(config.SCSS +'/*.scss')
-		.pipe(sass())
-		.pipe(postcss(plugins))
-		.pipe(dest(config.CSS))
+    var plugins = [cssnext];
+    return src(config.SCSS + '/*.scss')
+        .pipe(sass())
+        .pipe(postcss(plugins))
+        .pipe(dest(config.CSS))
 });
-
+// compress css
 // Move the javascript files into our /src/js folder
 task('js', function() {
-	return src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js'])
-		.pipe(concat('vendor.js'))
-		.pipe(dest(config.JS))
+    return src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js'])
+        .pipe(concat('vendor.js'))
+        .pipe(dest(config.JS))
 });
 
 // Watches scss files
 task('watch', series('sass', function(done) {
-	watch([config.SCSS + '/*.scss', config.SCSS + '/_*.scss'], series('sass'));
-	done();
+    watch([config.SCSS + '/*.scss', config.SCSS + '/_*.scss'], series('sass'));
+    done();
 }));
 
 task('build', series(config.buildTasks));
